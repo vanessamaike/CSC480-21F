cc27edbcecc65d84a4937c4ac602bf99ce0915f0
DatTran34 <tridat1vn@gmail.com>
2021-10-01 20:44:29 -0400

    create User Object and update Course Object

---
+++ backend/src/main/java/edu/oswego/rest/objects/User.java
@@ -0,0 +1,52 @@
+package edu.oswego.rest.objects;
+
+public class User {
+    private int userID;
+    private String email;
+    private String role;
+
+    public User(int userID, String email,String role){
+        this.userID = userID;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User(int userID){
+        User user = null ;//dbi.getAssignment(assignmentID);
+        if(user!=null){
+            this.userID = userID;
+            this.email = user.email;
+            this.role = user.role;
+        }
+        else {
+            this.userID = 1;
+            this.email = "";
+            this.role = "";
+        }
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
