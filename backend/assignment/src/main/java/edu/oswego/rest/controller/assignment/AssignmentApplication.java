f357210729d76c82988c6c1d5313f1bec55c5118
DatTran34 <tridat1vn@gmail.com>
2021-10-06 23:04:19 -0400

    create ASSIGNMENT micro-service

---
+++ backend/assignment/src/main/java/edu/oswego/rest/controller/assignment/AssignmentApplication.java
@@ -0,0 +1,9 @@
+package edu.oswego.rest.controller.assignment;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class AssignmentApplication extends Application {
+
+}
---
+++ backend/assignment/src/main/java/edu/oswego/rest/health/HelloReadinessCheck.java
@@ -0,0 +1,25 @@
+package edu.oswego.rest.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class HelloReadinessCheck implements HealthCheck {
+
+    private boolean isReady() {
+        // perform readiness checks, e.g. database connection, etc.
+
+        return true;
+    }
+	
+    @Override
+    public HealthCheckResponse call() {
+        boolean up = isReady();
+        return HealthCheckResponse.named(this.getClass().getSimpleName()).status(up).build();
+    }
+    
+}
