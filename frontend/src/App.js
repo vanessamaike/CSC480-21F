cadc146a0618ca2d34a073a24ebf98f37b3c6899
KoKo Aung <56900429+KoKoAung58@users.noreply.github.com>
2021-10-19 01:40:29 -0400

    assignment-creation-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,20 @@ import ProfessorHomeDashboard from './pages/Professor/ProfessorHomeDashboard';
 import TestingPage from './pages/TestingPage';
 import ProfessorCoursePage from './pages/Professor/ProfessorCoursePage';
 import AddCoursePage from './pages/Professor/AddCoursePage';
+import AssignmentCreation from './pages/Professor/AssignmentCreation';
 
 function App() {
   return (
-    
+
     <Router>
-        <Switch>
-          <Route exact path="/login" component={LoginPage}></Route>
-          <Route exact path="/home" component={ProfessorHomeDashboard}></Route>
-          <Route exact path="/testing" component={TestingPage}></Route>
-          <Route exact path="/course" component={ProfessorCoursePage}></Route>
-          <Route exact path="/addcourse" component={AddCoursePage}></Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/login" component={LoginPage}></Route>
+        <Route exact path="/home" component={ProfessorHomeDashboard}></Route>
+        <Route exact path="/testing" component={TestingPage}></Route>
+        <Route exact path="/course" component={ProfessorCoursePage}></Route>
+        <Route exact path="/addcourse" component={AddCoursePage}></Route>
+        <Route exact path="/assignmentcreation" component={AssignmentCreation}></Route>
+      </Switch>
     </Router>
   );
 }
--- frontend/src/pages/TestingPage.js
+++ frontend/src/pages/TestingPage.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import CustomizedTables from "../components/CustomizedTables";
 import CSVUploader from "../components/CSVUploader";
+import CustomizedRadios from "../components/CustomizedRadios";
 import CustomizedButtons from "../components/CustomizedButtons";
 import { Grid } from "@mui/material";
 
 function TestingPage() {
   const [jsonData, setjsonData] = useState([]);
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent:"center", width: "100%" , margin: "20px"}}>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", width: "100%", margin: "20px" }}>
       {/* <CSVUploader setjsonData={setjsonData}></CSVUploader>
       <CustomizedTables jsonData={jsonData.slice(0,-1)}></CustomizedTables> */}
       <Grid container spacing={2}>
         <Grid item>
           <CustomizedButtons type1 model={"type1"}>See All </CustomizedButtons>
+          <CustomizedRadios value></CustomizedRadios>
           <CustomizedButtons type1 model={"arrow"}>fsdfhui</CustomizedButtons>
         </Grid>
         <Grid item>
