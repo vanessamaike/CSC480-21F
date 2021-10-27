a3fb1cc6042bb6ecf2bf1fc554eb1215e67c4c30
ductran34 <dtran4@oswego.edu>
2021-10-26 23:41:59 -0400

    delete unused libraries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material/styles';
-import 'bootstrap/dist/css/bootstrap.css';
 //Whole website color
 //document.body.style = "background: #E5E5E5"
 
