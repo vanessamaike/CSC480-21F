1d4f7cbc04eb3ee5819484db068fc6082fb1ecd9
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2021-11-10 01:59:44 -0500

    Added assignment ID to Review
    Added assignment ID to Review

--- backend/util/src/main/java/edu/oswego/util/service/IReviewService.java
+++ backend/util/src/main/java/edu/oswego/util/service/IReviewService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface IReviewService{
     Review save(Review review);
     List<Review> findAll();
+    List<Review> findAllByAssId(int assId);
     Review findOne(int reviewId);
     Review update(Review review);
     Review delete(Review review);
