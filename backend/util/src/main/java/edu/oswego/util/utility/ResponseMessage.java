4dfae7e73819bf3ceeb0faa7ead88832dc6ab4a5
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2021-11-13 17:44:52 -0500

    Added ResponseMessage class
    Added the response message class that is to be used for generating the responses needed for GUI

---
+++ backend/util/src/main/java/edu/oswego/util/utility/ResponseMessage.java
@@ -0,0 +1,17 @@
+package edu.oswego.util.utility;
+
+import javax.ws.rs.core.Response;
+
+public class ResponseMessage {
+    public Response sendMessage(String message, int statusCode){
+        return Response
+                .status(statusCode)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .entity(message)
+                .build();
+    }
+}
