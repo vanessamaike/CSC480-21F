7954e44a18852abee980408d5f2229a225631df0
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2021-11-16 23:19:27 -0500

    Removed Answer directory
    This service is not used.

--- backend/answer/src/main/java/edu/oswego/rest/controller/response/IResponse.java
+++
@@ -1,9 +0,0 @@
-package edu.oswego.rest.controller.response;
-
-public interface IResponse {
-    ResponseModel response_Status_OK(Object result);
-    ResponseModel response_Status_Bad_Request(Object result);
-    ResponseModel response_Status_Unauthorized(Object result);
-    ResponseModel response_Status_Not_Found(Object result);
-    ResponseModel response_Status_Method_Not_Allowed(Object result);
-}
