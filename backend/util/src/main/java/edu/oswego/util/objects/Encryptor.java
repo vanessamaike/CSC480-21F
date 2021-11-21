7abb9bd9aed099a2dfdfe3238673a5e302ec24b5
andy <hayesandy5@gmail.com>
2021-11-21 17:05:50 -0500

    updated encryptor

--- backend/util/src/main/java/edu/oswego/util/objects/Encryptor.java
+++ backend/util/src/main/java/edu/oswego/util/objects/Encryptor.java
@@ -4,17 +4,23 @@ import com.google.crypto.tink.aead.AeadConfig;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.GeneralSecurityException;
 
 
 public class Encryptor {
     private static final String secret = "${encryption.secret}"; //Uses open liberty environment variable
+    private static final String file = "encryptionKey.txt";
     private KeysetHandle keysetHandle;
 
     //Create encryptor object from keyset file
-    public Encryptor(String filename) throws GeneralSecurityException {
+    public Encryptor() throws GeneralSecurityException {
+        if(!Files.exists(Path.of(file)))
+            createKey();
+
         try {
-            keysetHandle = CleartextKeysetHandle.read(JsonKeysetReader.withFile(new File(filename)));
+            keysetHandle = CleartextKeysetHandle.read(JsonKeysetReader.withFile(new File(file)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -22,9 +28,9 @@ public class Encryptor {
     }
 
     //create new json key file
-    public static void createKey(String filename) throws GeneralSecurityException {
+    public static void createKey() throws GeneralSecurityException {
         KeysetHandle kh = KeysetHandle.generateNew(KeyTemplates.get("AES128_GCM"));
-        File f = new File(filename);
+        File f = new File(file);
         try {
             CleartextKeysetHandle.write(kh, JsonKeysetWriter.withFile(f));
         } catch(IOException e) {
@@ -35,8 +41,7 @@ public class Encryptor {
     //encrypt string to byte array
     public byte[] encrypt(String plaintext) throws GeneralSecurityException {
         Aead aead = keysetHandle.getPrimitive(Aead.class);
-        byte[] ciphertext = aead.encrypt(plaintext.getBytes(),secret.getBytes());
-        return ciphertext;
+        return aead.encrypt(plaintext.getBytes(),secret.getBytes());
 
     }
 
