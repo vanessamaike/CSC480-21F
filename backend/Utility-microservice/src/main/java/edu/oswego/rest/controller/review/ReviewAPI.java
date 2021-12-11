42511a980a80e156d7bd8c3c7a3a5708573b5869
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2021-12-11 14:05:31 -0500

    QC import fixed

--- backend/Utility-microservice/src/main/java/edu/oswego/rest/controller/review/ReviewAPI.java
+++ backend/Utility-microservice/src/main/java/edu/oswego/rest/controller/review/ReviewAPI.java
@@ -1,61 +1,41 @@
 package edu.oswego.rest.controller.review;
 
-import edu.oswego.util.objects.Assignment;
+import edu.oswego.rest.utility.QualityCheck;
 import edu.oswego.util.objects.Review;
 import edu.oswego.util.objects.Student;
 import edu.oswego.util.objects.authObject;
-import edu.oswego.util.service.IAssignmentService;
-import edu.oswego.util.service.ICourseService;
 import edu.oswego.util.service.IReviewService;
 import edu.oswego.util.service.IStudentService;
-import edu.oswego.util.service.impl.AssignmentService;
-import edu.oswego.util.service.impl.CourseService;
 import edu.oswego.util.service.impl.ReviewService;
 import edu.oswego.util.service.impl.StudentService;
-import edu.oswego.util.utility.QualityCheck;
 import edu.oswego.util.utility.ResponseMessage;
-import edu.oswego.util.utility.SD;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 // Json-B
-import javax.json.*;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
 // JAX-RS
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.io.*;
 
-import java.nio.file.Files;
-
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Path("/review")
 public class ReviewAPI {
     private static final long serialVersionUID = 1L;
-    private final IAssignmentService assService;
     private final IReviewService reviewService;
     private final IStudentService studentService;
-    private final ICourseService courseService;
     private final Jsonb jsonb = JsonbBuilder.create();
-    private final String unauth = "Unauthorized: You mus log in to view this page.";
-    private final String forbid = "Forbidden: You do not have access to this page.";
-    private final String errorm = "An error occurred: ";
-    private final String badnum = "The ID number provided was not formatted properly.";
-    private authObject auth = new authObject() ;
+    private final authObject auth = new authObject() ;
 
-    private List<String> ROLE = Arrays.asList("professor","student");
+    private final List<String> ROLE = Arrays.asList("professor","student");
 
     public ReviewAPI() {
-        assService = new AssignmentService();
         reviewService = new ReviewService();
         studentService = new StudentService();
-        courseService = new CourseService();
     }
 
 
@@ -79,7 +59,7 @@ public class ReviewAPI {
 
         String token = obj.getString("token");
 
-        if(auth.isAuthenticated(token,ROLE) == false){
+        if(!auth.isAuthenticated(token, ROLE)){
             return new ResponseMessage().sendMessage("NotAuthenticated",404);
         }
 
@@ -108,6 +88,7 @@ public class ReviewAPI {
                     res.put(reviewJSON);
 
                 } catch (Exception e) {
+                    String errorm = "An error occurred: ";
                     return new ResponseMessage().sendMessage(errorm + e, 500);
                 }
             }
